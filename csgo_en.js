55dbb7511f0f9e0590c1d9b2898531c9c805872b
ard50500 <69609561+ard50500@users.noreply.github.com>
2023-02-28 22:38:39 +0300

    Update csgo_en.js

--- csgo_en.js
+++ csgo_en.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const { Server } = require('steam-query');
-const server = new Server('sunucu_ip_adresi', sunucu_portu); // CS:GO sunucusu IP adresi ve bağlantı noktası
+const server = new Server('server_ip_address', server_port); // IP address and port of the CS:GO server
 
 const client = new Discord.Client();
-const prefix = "!"; // Değiştirebileceğiniz önek
+const prefix = "!"; // The prefix that triggers the bot
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,14 +12,14 @@ client.on('ready', () => {
 client.on('message', async message => {
   if (message.content === prefix + 'csgo') {
     try {
-      const data = await server.getPlayers(); // Sunucudaki oyuncuların verilerini alın
-      const playerCount = data.players.length; // Oyuncu sayısını alın
-      message.channel.send(`CS:GO sunucusunda ${playerCount} oyuncu var.`); // Oyuncu sayısını kanala gönderin
+      const data = await server.getPlayers(); // Get data about the players on the server
+      const playerCount = data.players.length; // Get the number of players
+      message.channel.send(`There are ${playerCount} players on the CS:GO server.`); // Send the player count to the channel
     } catch (error) {
-      message.channel.send('CS:GO sunucusuna bağlanırken bir hata oluştu.');
+      message.channel.send('An error occurred while connecting to the CS:GO server.');
       console.error(error);
     }
   }
 });
 
-client.login('BOT_TOKEN'); // Discord botunuzun token'ını buraya yerleştirin.
+client.login('BOT_TOKEN'); // Replace BOT_TOKEN with your Discord bot token.
